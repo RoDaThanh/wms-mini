aee918e99dfe97934034bf6d37b1b4d46dfa842f
thanh.ro <thanhroda1@gmail.com>
2025-08-25 23:40:25 +0700

    feat: mock data to display

--- src/main/java/org/practice/servlets/service/ItemService.java
+++ src/main/java/org/practice/servlets/service/ItemService.java
@@ -5,6 +5,7 @@ import org.practice.servlets.entity.Item;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
 import java.util.List;
 
 @Stateless
@@ -13,16 +14,20 @@ public class ItemService {
     @PersistenceContext(unitName = "elmsPU")
     private EntityManager em;
 
+    private List<Item> itemsMock = new ArrayList<>();
+
 
     public Item addItem(String sku, String name) {
         Item item = new Item();
         item.setSKU(sku);
         item.setName(name);
-        em.persist(item);
+//        em.persist(item);
+        itemsMock.add(item);
         return item;
     }
 
     public List<Item> findAll() {
-        return em.createQuery("SELECT i FROM Item i", Item.class).getResultList();
+        return itemsMock;
+//        return em.createQuery("SELECT i FROM Item i", Item.class).getResultList();
     }
 }
